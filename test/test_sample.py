6e87d68ef8a567e77f84123d14697e0764f50838
janeusz2000 <mateusz.krzysztof.janik@gmail.com>
2020-12-18 13:45:44 +0100

    testing opening wav with linux

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,5 +1,8 @@
-
+import soundfile
 
 class TestClass:
     def test_baby(self):
         assert True == True
+        
+def test_openRecoring():
+    data, samplerate = soundfile.read("testRecordings/testRecording.wav")
