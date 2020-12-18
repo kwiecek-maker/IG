6f0f90a891c455ab197992cc31d380f524b43f01
Katarzyna Więcek <k.wiecek96@gmail.com>
2020-12-18 13:23:40 +0100

    improvement of MFCC algorithm's performance

--- test/test_mfcc.py
+++ test/test_mfcc.py
@@ -2,14 +2,13 @@
 comparision mfcc function from python_speech_features library with own implementation
 
 """
-
 from scipy.io import wavfile
 from python_speech_features import mfcc
 import numpy as np
 from matplotlib import pyplot as plt
-from repo.main.featureExtractor import MFCC
+from main.featureExtractor import MFCC
 
-fs, audioData = wavfile.read(r"wyłącz.wav")
+fs, audioData = wavfile.read(r"test/wyłącz.wav")
 
 segmentTime = 0.02
 segmentOverlap = 0.01
@@ -25,7 +24,7 @@ numberOfSegments = int(audioDataLen/samplesOverlap)-1
 
 audioDataSegments = np.zeros((samplesPerSegment, numberOfSegments))
 for i in range(numberOfSegments):
-    audioDataSegments[:, i] += (audioData[i * samplesOverlap: i * samplesOverlap + samplesPerSegment])
+    audioDataSegments[:, i] += (audioData[i*samplesOverlap:i*samplesOverlap+samplesPerSegment])
 
 obj = MFCC(audioDataSegments, samplerate=fs, numberOfCepstras=13, numberOfMelFilters=26,
            numberOfFrequencyBins=1024)
@@ -35,4 +34,4 @@ mfccFeaturesTest = mfccFeaturesTest.flatten('F')
 plt.plot(mfccFeaturesRef, label='referencyjne współczynniki mel-cepstralne')
 plt.plot(mfccFeaturesTest, c='red', label='obliczone współczynniki mel-cepstralne')
 plt.legend()
-plt.show()
+plt.savefig('test/acquiredData/mfcc.png', format='png')
