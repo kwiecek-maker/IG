2520f0b31a58bcf1d089048049ff69bacb7d028d
Zuzanna <suzanne.piatek@gmail.com>
2021-01-21 23:08:48 +0100

    Implemented LPC

--- SpeechGenSrc/LPCcoder.py
+++ SpeechGenSrc/LPCcoder.py
@@ -1,31 +1,51 @@
 import numpy as np
+import scipy.signal
 import os
 
-class LPCcoder:
 
-    def __init__(self, predictionLength, blockSize):
+class LPCcoder:
+    def __init__(self, predictionLength, frameSize):
         self.predictionLength = predictionLength
-        self.blockSize = blockSize
+        self.frameSize = frameSize
 
     def __repr__(self):
-        return "LPC coder. prediction Length: %d, BLock size: %d" % (self.predictionLength, self.blockSize)
+        return "LPC coder. prediction Length: %d, BLock size: %d" % (self.predictionLength, self.frameSize)
     
     def __str__(self):
         return self.__repr__()
 
-    def codeRrcording(self, inputRecording):
+    def codeRecording(self, inputRecording, fs):
         signalLength = np.size(inputRecording)
-        numOfBlocks = np.int(np.floor(signalLength / self.blockSize))
-        
+        frameSamples = np.int(np.floor(signalLength * self.frameSize))
+        numOfBlocks = np.int(np.floor(signalLength / frameSamples))
+
         predictionCoefficientMemory = np.zeros((numOfBlocks, self.predictionLength))
         predictionError = np.zeros(signalLength)
 
-        return [predictionCoefficientMemory, predictionError]
+        # Memory state of prediction filter
+        stateOfPredictionFilter = np.zeros(self.predictionLength)
+
+        for i in range(0, numOfBlocks):
+            # Temporary matrix, which contain x rates of filter for each sample in frame
+            A = np.zeros((frameSamples - self.predictionLength, self.predictionLength))
+
+            for j in range(0, frameSamples - self.predictionLength):
+                A[j, :] = np.flipud(inputRecording[i * frameSamples + j + np.arange(self.predictionLength)])
+
+            # Construct our desired target signal d, one sample into the future
+            d = inputRecording[i * frameSamples + np.arange(self.predictionLength, frameSamples)]
+            # Compute the prediction filter
+            predictionCoefficientMemory[i, :] = np.dot(np.dot(np.linalg.pinv(np.dot(A.transpose(), A)), A.transpose()), d)
+            hperr = np.hstack([1, -predictionCoefficientMemory[i, :]])
+            predictionError[i * frameSamples + np.arange(0, frameSamples)], stateOfPredictionFilter = scipy.signal.lfilter(hperr, [1], inputRecording[i * frameSamples + np.arange(0, frameSamples)], zi=stateOfPredictionFilter)
+
+        return predictionCoefficientMemory, predictionError, inputRecording.shape, frameSamples, numOfBlocks
 
     # Exports calculated LPC coefficients into txt files in speechGenSrc/dataBase/
-    def exportLPC(self, recordingName, predictionCoefficientMemory, predicionError):
+    @staticmethod
+    def exportLPC(recordingName, predictionCoefficientMemory, predictionError):
         with open(os.getcwd() + "\\SpeechGenSrc\\database\\" + recordingName + "predictionCoefficientMemory.txt", 'w+') as f:
-            f.writelines("predictionCoefficientMemory")
+            f.writelines(predictionCoefficientMemory)
 
         with open(os.getcwd() + "\\SpeechGenSrc\\database\\" + recordingName + "predictionError.txt", 'w+') as f:
-            f.writelines("predictionError")
+            f.writelines(predictionError)
--- SpeechGenSrc/decode.py
+++ SpeechGenSrc/decode.py
@@ -5,38 +5,75 @@ $ python decode.py "command"
 It will automaticly play decoded signal.
 """
 
-import sys
+import numpy as np
+import scipy.signal
+import soundfile
+import simpleaudio
+
 
 # change given name to the associated LPC coefficient file and LPC coefficient error
 def transposeName(name):
     return ["", ""]
 
-# walks through directories and read files associated with 
+
+# walks through directories and read files associated with
 # given filename. Returns None when files are not found.
 def readTxtfiles(filename):
     return None
 
+
 # decode txt files from database files
 class Decoder:
 
-    def __init__(self, LPCmatrix, LPCerror):
-        self.LPCmatrix = LPCMatrix
+    def __init__(self, LPCmatrix, LPCerror, inputRecordingShape, frameSamples, numOfBlocks, fs, predictionLength):
+        self.LPCmatrix = LPCmatrix
         self.LPCerror = LPCerror
-    
-    def generateRecording(self):
-        return False
-    
-    def playRecording(self):
-        return False
+        self.inputRecordingShape = inputRecordingShape
+        self.frameSamples = frameSamples
+        self.numOfBlocks = numOfBlocks
+        self.fs = fs
+
+        self.predictionLength = predictionLength
+
+    # Decoder
+    def decodeRecording(self):
+        # Initialize reconstructed signal memory
+        synthesisRecording = np.zeros(self.inputRecordingShape)
+        # Initialize Memory state of prediction filter
+        state = np.zeros(self.predictionLength)
+
+        for i in range(0, self.numOfBlocks):
+            # Predictive reconstruction filter: hperr from numerator to denominator
+            hperr = np.hstack([1, -self.LPCmatrix[i, :]])
+            synthesisRecording[i * self.frameSamples + np.arange(0, self.frameSamples)], state = scipy.signal.lfilter([1], hperr, self.LPCerror[i * self.frameSamples + np.arange(0, self.frameSamples)], zi=state)
+
+        return synthesisRecording
+
+    def generateRecording(self, outputPath, synthesisRecording):
+        soundfile.write(outputPath, synthesisRecording, self.fs)
+
+    @staticmethod
+    def playRecording(wavFilePath):
+        waveObj = simpleaudio.WaveObject.from_wave_file(wavFilePath)
+        playObj = waveObj.play()
+
+        while True:
+            if playObj.is_playing():
+                print('Playing...')
+            else:
+                print('Ended')
+                break
+
 
 if __name__ == "__main__":
-    name = sys.argv[1]
-    [LPCmatrixName, LPCerrrorName] = transposeName(name)
+    pass
+    # name = sys.argv[1]
+    # [LPCmatrixName, LPCerrrorName] = transposeName(name)
+    #
+    # LPCMatrix = readTxtfiles(LPCmatrixName)
+    # LPCError = readTxtfiles(LPCerrrorName)
 
-    LPCMatrix = readTxtfiles(LPCmatrixName)
-    LPCError = readTxtfiles(LPCerrrorName)
-
-    if LPCMatrix is not None and LPCError is not None:
-        decoder = Decoder(LPCMatrix, LPCError)
-        decoder.generateRecording()
-        decoder.playRecording()
+    # if LPCMatrix is not None and LPCError is not None:
+    #     decoder = Decoder(LPCMatrix, LPCError)
+    #     decoder.decodeRecording()
+    #     decoder.generateRecording()
